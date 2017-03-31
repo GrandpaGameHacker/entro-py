6be0d65c6e158fb972cb0bb40dff974d1affc2f0
samsonpianofingers <itsthatguyagain3@gmail.com>
2017-03-31 19:42:48 +1100

    added project files
    Entropy!

---
+++ ent.py
@@ -0,0 +1,87 @@
+"""
+This module contains functions that measure amount of information
+entropy in byte-type buffers using the shannon entropy algorthmn
+Reference: - https://en.wikipedia.org/wiki/Shannon_entropy
+Algorithm based on C++ code from x64dbg project at
+Reference: - https://github.com/x64dbg/x64dbg/blob/development/src/gui/Src/QEntropyView/Entropy.h
+"""
+import math
+
+def ent(buffer):
+	"""Measure the entropy of a group of bytes
+	Returns a floating point entropy value between 0 and 1
+
+	Keyword arguments:
+	buffer -- a byte like buffer (bytearray, bytes) to measure
+	"""
+	occurrences = [];
+	for i in range(0,256):
+		occurrences.append(0)
+	for x in buffer:
+		occurrences[x]+=1
+	entropy = 0
+	logbase = math.log(256)
+	p = 0
+	for i in range(0,256):
+		if(occurrences[i] == 0):
+			continue
+		p = float(occurrences[i] / float(len(buffer)))
+		entropy += p * math.log(p) / logbase
+	return -entropy
+
+	
+def listent(buffer, blocksize):
+	"""Measure the entropy of a group of bytes in blocks
+	Returns a list of values between 0 and 1
+	
+	Keyword arguments:
+	buffer -- a byte like buffer (bytearray, bytes) to measure
+	blocksize -- integer number defining how large each block is.
+	"""
+	nblocks = int(math.trunc(len(buffer)/blocksize))
+	if(nblocks == 0):
+		return
+	if(nblocks == 1):
+		return
+	entropylist = []
+	entropylist.append(ent(buffer[0:blocksize]))
+	currentblock = 0
+	nextblock = blocksize
+	for i in range(nblocks):
+		entropylist.append(ent(buffer[currentblock:nextblock]))
+		currentblock+=blocksize
+		nextblock+=blocksize
+	return entropylist
+
+
+def bitent(buffer):
+	"""Measure the entropy of a group of bytes
+	Returns a floating point entropy value between 0 and 8 (bits per byte)
+
+	Keyword arguments:
+	buffer -- a byte like buffer (bytearray, bytes) to measure
+	"""
+	return ent(buffer)*8
+
+def listbitent(buffer, blocksize):
+	"""Measure the entropy of a group of bytes in blocks
+	Returns a list of values between 0 and 8 (bits per byte)
+	
+	Keyword arguments:
+	buffer -- a byte like buffer (bytearray, bytes) to measure
+	blocksize -- integer number defining how large each block is.
+	"""
+	nblocks = int(math.trunc(len(buffer)/blocksize))
+	if(nblocks == 0):
+		return 0
+	if(nblocks == 1):
+		return ent(buffer[0:blocksize])
+	entropylist = []
+	currentblock = 0
+	nextblock = blocksize
+	for i in range(nblocks):
+		entropylist.append(ent(buffer[currentblock:nextblock])*8)
+		currentblock+=blocksize
+		nextblock+=blocksize
+	return entropylist
+
---
+++ entfile.py
@@ -0,0 +1,29 @@
+"""
+This module contains functions that wrap ent.py for use with files
+"""
+import ent as e
+
+def ent(file):
+	f = open(file,"rb")
+	bytes = f.read()
+	f.close()
+	return e.ent(bytes)
+	
+def listent(file, blocksize):
+	f = open(file,"rb")
+	bytes = f.read()
+	f.close()
+	return e.listent(bytes, blocksize)
+
+
+def bitent(file):
+	f = open(file,"rb")
+	bytes = f.read()
+	f.close()
+	return e.ent(bytes)
+
+def listbitent(file, blocksize):
+	f = open(file,"rb")
+	bytes = f.read()
+	f.close()
+	return e.listbitent(bytes, blocksize)
